93ce56e3b539763df754b5137b7f3c701f34f189
Luke Hansford <l.s.hansford@gmail.com>
2014-09-22 12:22:59 +1000

    Added thumbnail search for collections

--- models.py
+++ models.py
@@ -32,13 +32,13 @@ class Collection(db.Model):
 	items = db.relationship('CollectionItem', backref='collection', lazy='dynamic')
 
 	def get_thumbnail(self):
-		if self.thumbnail:
+		if self.thumbnail and self.thumbnail != "":
 			return self.thumbnail
-		else:
-			for i in items:
-				a = Article.query.get(i.article_id)
-				if a.lead_image:
-					return a.lead_image
+		# else:
+		# 	for i in items:
+		# 		a = Article.query.get(i.article_id)
+		# 		if a.lead_image:
+		# 			return a.lead_image
 		return "http://ruon.tv/wp-content/uploads/2014/02/default-image.png"
 
 	def items_dict(self):
